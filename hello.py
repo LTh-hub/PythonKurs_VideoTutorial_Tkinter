50f9022b2561f381c525f7e1adc78d51551bed4e
Lars Thomsson <lartho63@gmail.com>
2025-04-18 11:10:55 +0200

    1) Create Graphical User Interface with Python and Tkinter (hello.py)

---
+++ hello.py
@@ -0,0 +1,22 @@
+# hello.py
+#
+# YouTube :: Python GUI's With Tkinter
+#            CODEMY.COM - Learn to code
+#
+# 1) Create Graphical User Interface with Python and Tkinter
+#   
+#
+from tkinter import *
+
+root = Tk()
+
+myLabel = Label(root, text="Hello World!")
+myLabel.pack(pady=10)
+
+
+root.mainloop()
+
+
+
+
+
