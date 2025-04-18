83846ad9ab65a77c927ebbc4786be58c6ad85827
Lars Thomsson <lartho63@gmail.com>
2025-04-18 12:31:52 +0200

    5.2) Build a Simple Calculator App - Python Tkinter GUI #5 (calculator.py)

--- calculator.py
+++ calculator.py
@@ -7,6 +7,7 @@
 # 2) Positioning WithTkinter Grid System - Python Tkinter GUI #2
 # 3) Creating Buttons With Tkinter - Python Tkinter GUI #3
 # 4) Creating Input Fields With Tkinter - Python Tkinter GUI #4
+# 5) Build a Simple Calculator App - Python Tkinter GUI #5
 #
 #
 from tkinter import *
@@ -20,23 +21,47 @@ e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
 
-def button_add():
+def button_click():
     return
 
 
-button_1 = Button(root, text="1", padx=40, pady=20, command=button_add)
-button_2 = Button(root, text="2", padx=40, pady=20, command=button_add)
-button_3 = Button(root, text="3", padx=40, pady=20, command=button_add)
-button_4 = Button(root, text="4", padx=40, pady=20, command=button_add)
-button_5 = Button(root, text="5", padx=40, pady=20, command=button_add)
-button_6 = Button(root, text="6", padx=40, pady=20, command=button_add)
-button_7 = Button(root, text="7", padx=40, pady=20, command=button_add)
-button_8 = Button(root, text="8", padx=40, pady=20, command=button_add)
-button_9 = Button(root, text="9", padx=40, pady=20, command=button_add)
-button_0 = Button(root, text="0", padx=40, pady=20, command=button_add)
+# Define buttons
+button_1 = Button(root, text="1", padx=40, pady=20, command=button_click)
+button_2 = Button(root, text="2", padx=40, pady=20, command=button_click)
+button_3 = Button(root, text="3", padx=40, pady=20, command=button_click)
+button_4 = Button(root, text="4", padx=40, pady=20, command=button_click)
+button_5 = Button(root, text="5", padx=40, pady=20, command=button_click)
+button_6 = Button(root, text="6", padx=40, pady=20, command=button_click)
+button_7 = Button(root, text="7", padx=40, pady=20, command=button_click)
+button_8 = Button(root, text="8", padx=40, pady=20, command=button_click)
+button_9 = Button(root, text="9", padx=40, pady=20, command=button_click)
+button_0 = Button(root, text="0", padx=40, pady=20, command=button_click)
+
+button_add = Button(root, text="+", padx=39, pady=20, command=button_click)
+button_equal = Button(root, text="=", padx=91, pady=20, command=button_click)
+button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_click)
+
 
 
 #Put buttonas on screen
+
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+
+button_0.grid(row=4, column=0)
+button_clear.grid(row=4, column=1, columnspan=2)
+button_add.grid(row=5, column=0)
+button_equal.grid(row=5, column=1, columnspan=2)
+
 
 
 #myButton = Button(root, text="Ange ditt namn", command=myClick, fg="blue", bg="white")
