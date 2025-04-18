47740c58e6275a046a96e6efd20df4ef3e5a1ea7
Lars Thomsson <lartho63@gmail.com>
2025-04-18 13:30:46 +0200

    8.1) Using Icons, Images, and Exit Buttons - Python Tkinter GUI #8 (images.py)

---
+++ images.py
@@ -0,0 +1,29 @@
+# images.py
+#
+# YouTube :: Python GUI's With Tkinter
+#            CODEMY.COM - Learn to code
+#
+# 1) Create Graphical User Interface with Python and Tkinter
+# 2) Positioning WithTkinter Grid System - Python Tkinter GUI #2
+# 3) Creating Buttons With Tkinter - Python Tkinter GUI #3
+# 4) Creating Input Fields With Tkinter - Python Tkinter GUI #4
+# 5) Build a Simple Calculator App - Python Tkinter GUI #5
+# 6) Continuing Build a Simple Calculator App - Python Tkinter GUI #6
+# 7) Finish Build a Simple Calculator App - Python Tkinter GUI #7
+# 8) Using Icons, Images, and Exit Buttons - Python Tkinter GUI #8
+#
+#
+from tkinter import *
+
+root = Tk()
+root.title("Learn to Code at Codemy.com")
+root.geometry("500x350")
+
+
+
+
+
+
+
+
+root.mainloop()
