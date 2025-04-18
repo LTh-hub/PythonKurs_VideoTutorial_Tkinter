f51c9e891aedbb7ae4048c16cc5638d9ca1bb928
Lars Thomsson <lartho63@gmail.com>
2025-04-18 11:52:49 +0200

    4) Creating Input Fields With Tkinter - Python Tkinter GUI #4 (entry.py)

---
+++ entry.py
@@ -0,0 +1,41 @@
+# entry.py
+#
+# YouTube :: Python GUI's With Tkinter
+#            CODEMY.COM - Learn to code
+#
+# 1) Create Graphical User Interface with Python and Tkinter
+# 2) Positioning WithTkinter Grid System - Python Tkinter GUI #2
+# 3) Creating Buttons With Tkinter - Python Tkinter GUI #3
+# 4) Creating Input Fields With Tkinter - Python Tkinter GUI #4
+#
+#
+from tkinter import *
+
+root = Tk()
+root.title("Learn to Code")
+root.geometry("500x350")
+
+
+e = Entry(root, width=50, borderwidth=8)
+e.pack(pady=10)
+
+
+def myClick():
+    myLabel = Label(root, text="Look, I clicked a button!!")
+    myLabel.pack()
+
+
+myButton = Button(root, text=" - Click me!", command=myClick, fg="blue", bg="white")
+myButton.pack()
+
+
+
+
+
+
+root.mainloop()
+
+
+
+
+
